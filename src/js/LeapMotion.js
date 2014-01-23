93f2af58e62d9175c73ced0e81ce40863918055e
mone <simone.fabiano@gmail.com>
2014-01-23 12:12:56 +0100

    update copyright and add missing licnese

--- src/js/LeapMotion.js
+++ src/js/LeapMotion.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 Weswit s.r.l.
+Copyright 2014 Weswit s.r.l.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
