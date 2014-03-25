bfcf7afda1504a1a75e1f26de68e4436d5efb9aa
mone <simone.fabiano@gmail.com>
2014-03-25 16:56:03 +0100

    zoom in&out the menu based on the window size

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -8,18 +8,53 @@ define(function() {
   var hideTop = 0;
   var originalLogoSize = 228;
   var targetLogoSize = 100;
+  
+  var topPosition = 0;
+  var leftPosition = 0;
+  var originalHeight = null;
+  var originalWidth = null;
+  var originalLinkHeightRow = null;
+  var firstSetup = true;
+  
+  var zoom = 1;
   $("#logo").css("height",originalLogoSize+"px");
   
   
   return {
     
     setup: function() {
+      if (firstSetup) {
+        originalHeight = $("#tools").outerHeight();
+        originalWidth = $("#tools").outerWidth();
+        originalStuffHeight = $("#tools_stuff").outerHeight();
+        originalLinkHeightRow = $("#linksRow").outerHeight();
+        originalHideTop = -originalStuffHeight+originalLogoSize+20+originalLinkHeightRow;
+        firstSetup = false;
+      };
+    
+
+      zoom=1;
+      while (zoom > 0 && originalHeight*zoom > window.innerHeight) {
+        zoom -= 0.1;
+      }
+      while (zoom > 0 && originalWidth*zoom > window.innerWidth) {
+        zoom -= 0.1;
+      }
+      
+      topPosition = (originalHeight*zoom - originalHeight)/2;
+      leftPosition = (window.innerWidth-originalWidth)/2;
+      
+      
+      $("#tools").css("transform","scale("+zoom+", "+zoom+")");
+      $("#tools").css("left",leftPosition);
+      $("#tools").css("top",topPosition);
+      
+      
+      hideTop = topPosition + originalHideTop*zoom + (1-zoom)*90;
       
       if (!isOpen) {
-        hideTop =  -$("#tools_stuff").outerHeight()+targetLogoSize+20;
-        $("#tools").css("top",hideTop);
-      } else {
-        hideTop =  -$("#tools_stuff").outerHeight()+originalLogoSize+20+$("#linksRow").outerHeight();
+        isOpen = true;
+        this.close();
       }
     },
     isOpen: function() {
@@ -28,8 +63,8 @@ define(function() {
     open: function() {
       if (!isOpen) {
         $("#linksRow").show();
-        $("#tools").css("top",0);
-        $("#logo").css("height",originalLogoSize+"px");
+        $("#tools").css("top",topPosition);
+          $("#logo").css("height",originalLogoSize+"px");
         $("#tools_button").attr("src","images/up.png");
         isOpen = true;
       }
